514f02b8c4be94727f9eb5ba463b5d4205709b85
Maxime Zupka <zupka.maxime@gmail.com>
2019-07-01 23:40:11 +0200

    test

--- Code/AtlantisConsole/UnitTestsNet/UnitTest1.cs
+++ Code/AtlantisConsole/UnitTestsNet/UnitTest1.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using AtlantisConsole.Calculation;
+using AtlantisConsole.Model;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UnitTestsNet
@@ -6,9 +9,19 @@ namespace UnitTestsNet
     [TestClass]
     public class UnitTest1
     {
+        ComplexCalculation complexCalculation = new ComplexCalculation();
+
         [TestMethod]
-        public void TestMethod1()
+        public void AveragesTest()
         {
+            double[] metric = new double[] { 12, 10 };
+
+            var calc = complexCalculation.Averages(metric);
+
+            double expected = 11;
+
+            Assert.AreEqual(expected, calc);
         }
+
     }
 }
